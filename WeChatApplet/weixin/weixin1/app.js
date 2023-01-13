2ace3ade8f1377ba8a06bf23341913e92824f224
xqkillua <524971307@qq.com>
2023-01-13 17:37:31 +0800

    no message

--- WeChatApplet/weixin/weixin1/app.js
+++ WeChatApplet/weixin/weixin1/app.js
@@ -1,7 +1,8 @@
 // app.js
 App({
+  //启动时先做一些事情
   onLaunch() {
-    console.log('初始化')
+    console.log('onLaunch =-= 初始化')
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -14,6 +15,7 @@ App({
       }
     })
   },
+  //全局公共方法、属性
   globalData: {
     userInfo: null
   }
